c1f7e15921859a7d5bc9470619f05ac6a9c7ac05
lazysheep <714201376@qq.com>
2017-12-10 19:09:24 +0800

    js lint

--- source/js/toggle_nav.js
+++ source/js/toggle_nav.js
@@ -1,36 +1,36 @@
-(function toggleNavModule() {
+(function toggleNavModule () {
   /**
- * toggle the nav
- * when in the mobile terminal
- */
-function toggleNav() {
-  const NAVS = document.getElementById('navs');
-  if(NAVS.classList.contains('in')) {
-    NAVS.classList.remove('in');
-  } else {
-    NAVS.classList.add('in');
+  * toggle the nav
+  * when in the mobile terminal
+  */
+  function toggleNav () {
+    const NAVS = document.getElementById('navs')
+    if (NAVS.classList.contains('in')) {
+      NAVS.classList.remove('in')
+    } else {
+      NAVS.classList.add('in')
+    }
   }
-}
 
-/**
- * hide the nav if click any part of the site 
- */
-function hideNav() {
-  const NAVS = document.getElementById('navs');
-  NAVS.classList.remove('in');
-}
-
-/**
- * main
- */
-const HTML = document.documentElement;
-HTML.addEventListener('click', (e) => {
-  const TOGGLE_NAV = document.getElementById('toggle-nav');
-  if(e.target === TOGGLE_NAV) {
-    toggleNav();
-  } else if (!e.target.classList.contains('site-page')) {
-    hideNav();
+  /**
+  * hide the nav if click any part of the site
+  */
+  function hideNav () {
+    const NAVS = document.getElementById('navs')
+    NAVS.classList.remove('in')
   }
-}, 
-false);
-})();
+
+  /**
+  * main
+  */
+  const HTML = document.documentElement
+  HTML.addEventListener('click', (e) => {
+    const TOGGLE_NAV = document.getElementById('toggle-nav')
+    if (e.target === TOGGLE_NAV) {
+      toggleNav()
+    } else if (!e.target.classList.contains('site-page')) {
+      hideNav()
+    }
+  },
+  false)
+})()
